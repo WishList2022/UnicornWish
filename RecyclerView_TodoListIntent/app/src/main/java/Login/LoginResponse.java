a9c44919cf892a396cb15424022644c42658cf46
Android No.1 <yeon8747@naver.com>
2022-07-20 21:15:27 +0900

    Auto stash before merge of "main" and "origin/main"

--- RecyclerView_TodoListIntent/app/src/main/java/Login/LoginResponse.java
+++ RecyclerView_TodoListIntent/app/src/main/java/Login/LoginResponse.java
@@ -1,4 +1,16 @@
 package Login;
 
 public class LoginResponse {
+
+    private String account_id;
+    private String password;
+
+    public String getAccount_id() {
+        return account_id;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
 }
