413a4f36b0f9a034765728c69c535e20dcfe12fe
Android No.1 <yeon8747@naver.com>
2022-08-06 23:55:42 +0900

    Post Token

--- RecyclerView_TodoListIntent/app/src/main/java/org/techtown/Activity/InfoActivity.java
+++ RecyclerView_TodoListIntent/app/src/main/java/org/techtown/Activity/InfoActivity.java
@@ -1,19 +1,13 @@
 package org.techtown.Activity;
 
-<<<<<<< HEAD
 import static android.content.ContentValues.TAG;
-
-=======
->>>>>>> main
 import android.content.Intent;
 import android.content.pm.ActivityInfo;
+import android.media.session.MediaSession;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
-<<<<<<< HEAD
 import android.widget.Switch;
-=======
->>>>>>> main
 import android.widget.Toast;
 
 import androidx.annotation.Nullable;
@@ -21,26 +15,22 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import org.techtown.Activity.databinding.ActivityItemInfoBinding;
 
-<<<<<<< HEAD
 import Api.ApiProvider;
 import Api.ServerApi;
 import Register.RegisterRequest;
-import WishPostRequest.PostRequest;
+import WishPost.PostRequest;
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
-=======
 import java.nio.charset.StandardCharsets;
 
 import Api.ApiProvider;
 import Api.ServerApi;
 import Login.LoginRequest;
-import WishPost.PostRequest;
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
 import retrofit2.http.POST;
->>>>>>> main
 
 public class InfoActivity extends AppCompatActivity {
 
@@ -53,7 +43,6 @@ public class InfoActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         binding = ActivityItemInfoBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
-<<<<<<< HEAD
 
 
         binding.infoBtnOk.setOnClickListener(new View.OnClickListener() {
@@ -66,33 +55,36 @@ public class InfoActivity extends AppCompatActivity {
         binding.btnNor.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                color = "WHITE";
+                color = "하얀색";
+                BtnPost();
             }
         });
 
         binding.btnRed.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                color = "RED";
+                color = "빨간색";
             }
         });
 
         binding.btnBlu.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                color = "BLUE";
+                color = "파란색";
             }
         });
+
         binding.btnGre.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                color = "GREEN";
+                color = "초록색";
             }
         });
+
         binding.btnYel.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                color = "YELLOW";
+                color = "노란색";
             }
         });
 
@@ -105,34 +97,21 @@ public class InfoActivity extends AppCompatActivity {
 
         PostRequest postRequest = new PostRequest(title, content, color);
         ServerApi serverApi = ApiProvider.getRetrofit().create(ServerApi.class);
-
-        serverApi.WishPost(postRequest).enqueue(new Callback<Void>() {
+        Class<LoginActivity> accessToken = LoginActivity.class;
+        serverApi.WishPost(accessToken, postRequest).enqueue(new Callback<Void>() {
             @Override
             public void onResponse(Call<Void> call, Response<Void> response) {
-                Log.d(TAG,"onResponse: "+response.code());
                 if (response.isSuccessful()){
-                    Toast.makeText(InfoActivity.this, "🎉Wish가 등록되었습니다!🎉", Toast.LENGTH_SHORT).show();
-                    Intent intent = new Intent(getApplicationContext(),MainActivity.class);
-                    finish();
-                }else{
-                    Toast.makeText(InfoActivity.this, "오류가 났습니다...", Toast.LENGTH_SHORT).show();
+                    binding.infoEtvTitle.setText("");
+                    binding.infoEtvContent.setText("");
+                    binding.btnNor.setText("");
+                    Toast.makeText(InfoActivity.this, "Wish가 등록 되었습니다.", Toast.LENGTH_SHORT).show();
                 }
-                finish();
             }
 
             @Override
             public void onFailure(Call<Void> call, Throwable t) {
-                Toast.makeText(InfoActivity.this, "통신 실패 하...", Toast.LENGTH_SHORT).show();
-                Log.d(TAG,"onFailure: "+t);
-            }
-        });
-=======
-        
-
-    binding.infoBtnOk.setOnClickListener(new View.OnClickListener() {
-        @Override
-        public void onClick(View view) {
-            Btn();
+                Toast.makeText(InfoActivity.this, "관리자에게 문의 해주세요..", Toast.LENGTH_SHORT).show();
             }
         });
 
@@ -143,67 +122,5 @@ public class InfoActivity extends AppCompatActivity {
             startActivity(intent);
         }
     });
-
-    binding.btnNor.setOnClickListener(new View.OnClickListener() {
-        @Override
-        public void onClick(View view) {
-            color = "WHITE";
-
-        }
-    });
-
-    binding.btnBlu.setOnClickListener(new View.OnClickListener() {
-        @Override
-        public void onClick(View view) {
-            color = "BLUE";
-        }
-    });
-
-    binding.btnGre.setOnClickListener(new View.OnClickListener() {
-        @Override
-        public void onClick(View view) {
-            color = "GREEN";
-        }
-    });
-
-
-    binding.btnRed.setOnClickListener(new View.OnClickListener() {
-        @Override
-        public void onClick(View view) {
-            color = "RED";
-        }
-    });
-
-    binding.btnYel.setOnClickListener(new View.OnClickListener() {
-        @Override
-        public void onClick(View view) {
-            color = "YELLOW";
-        }
-    });
-
     }
-
-
-    private void Btn(){
-        String tilte = binding.infoEtvTitle.getText().toString();
-        String content = binding.infoEtvContent.getText().toString();
-
-        if (tilte.length() == 0 || content.length() == 0){
-            Toast.makeText(InfoActivity.this, "모든 항목을 입력해주세요.", Toast.LENGTH_SHORT).show();
-        }else
-        {
-            BtnPost(tilte, content, color);
-        }
-    }
-
-    public void BtnPost(String tilte, String content, String color){
-
-
-        PostRequest postRequest = new PostRequest(tilte, content, color);
-        ServerApi serverApi = ApiProvider.getRetrofit().create(ServerApi.class);
-
-
->>>>>>> main
-    }
-
 }
