a76ea4fcb8468b7ac18d473e75aec937fa8ca184
Android No.1 <yeon8747@naver.com>
2022-08-06 23:56:05 +0900

    Post Request

--- RecyclerView_TodoListIntent/app/src/main/java/WishPost/PostRequest.java
+++ RecyclerView_TodoListIntent/app/src/main/java/WishPost/PostRequest.java
@@ -16,23 +16,11 @@ public class PostRequest {
         return title;
     }
 
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
     public String getContent() {
         return content;
     }
 
-    public void setContent(String content) {
-        this.content = content;
-    }
-
     public String getColor() {
         return color;
-    }
-
-    public void setColor(String color) {
-        this.color = color;
     }
 }
